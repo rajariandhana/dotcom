06556477e4cd5473e2854b1ad3359e0f8bf213a9
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2024-08-08 10:19:10 +0700

    com

--- resources/views/livewire/photo-filter.blade.php
+++ resources/views/livewire/photo-filter.blade.php
@@ -1,4 +1,15 @@
 <div>
+    @if ($bigPhoto != '')
+    <div class="fixed top-0 right-0 left-0 z-10 flex justify-center items-center w-full h-full">
+        <div wire:click="view('')" class="fixed top-0 right-0 left-0 z-10 flex justify-center items-center w-full h-full bg-black opacity-60">
+        </div>
+        <img
+            class="h-2/3 object-cover transition ease-in-out
+            fixed top-55 left-50 bg-gray-100 p-4  z-20"
+            src="{{ asset($bigPhoto) }}"
+            alt="{{$bigPhoto}}">
+    </div>
+    @endif
     <div class="flex mb-4 justify-center">
         <ul class="flex flex-row justify-center items-center gap-2">
             @foreach ($cities as $city)
@@ -31,8 +42,9 @@
     </div>
     <div class="flex flex-wrap justify-center gap-4 mt-8 lg:mt-12 lg:-mx-48">
         @foreach ($photos as $photo)
-            <img class="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40  rounded-md object-cover
-            hover:scale-110 transition ease-in-out"
+        <img wire:click="view('{{ '/Gallery/' . $photo->year . '_' . $photo->city . '/' . $photo->file }}')"
+            class="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40  rounded-md object-cover
+            hover:scale-110 transition ease-in-out cursor-pointer"
             src="{{ asset('/Gallery/' . $photo->year . '_' . $photo->city . '/' . $photo->file) }}"
             alt="/Gallery/' . $photo->year . '_' . $photo->city . '/' . $photo->file) }}">
         @endforeach
