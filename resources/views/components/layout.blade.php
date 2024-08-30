490428c83db421c76a6df20bc4496ccae3194d7a
rajariandhana <rajariandhana@gmail.com>
2024-08-30 22:23:08 +0700

    favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,7 @@ lang="en"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title}}</title>
+    <link rel="shortcut icon" href="{{asset('favicon.png')}}">
     {{-- @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js']) --}}
     @vite(['resources/css/app.css','resources/js/app.js'])
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
