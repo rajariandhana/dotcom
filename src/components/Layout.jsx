45b5fc0137ebb99632b5e4adb8e627bc141aa466
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2025-11-17 00:46:34 +1000

    feat: add footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import Nav from "./Nav";
+import Footer from "./Footer";
 import { Outlet } from "react-router";
 
 const Layout = () => {
@@ -7,10 +8,10 @@ const Layout = () => {
       className="flex flex-col min-h-screen gap-6 bg-stone-100"
     >
       <Nav/>
-      <main className="flex flex-col items-center justify-center w-full gap-12 px-12 pt-4 lg:gap-12">
+      <main className="flex flex-col items-center justify-center w-full gap-12 px-12">
         <Outlet />
       </main>
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 };
