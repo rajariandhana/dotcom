b431105af4ee16a7533db853c968fd3ddb0637ee
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2025-11-17 01:02:22 +1000

    feat: add under maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import { Routes, Route } from 'react-router'
 
 import Layout from './components/Layout'
 import Home from './pages/Home'
+import UnderMaintenance from './components/UnderMaintenance'
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" />
-        <Route path="/experience" />
-        <Route path="/photos" />
-        <Route path="/chatbot" />
-        <Route path="/settings" />
+        <Route path="/projects" element={<UnderMaintenance />} />
+        <Route path="/experience" element={<UnderMaintenance />} />
+        <Route path="/photos" element={<UnderMaintenance />} />
+        <Route path="/chatbot" element={<UnderMaintenance />} />
+        <Route path="/settings" element={<UnderMaintenance />} />
       </Route>
     </Routes>
   )
