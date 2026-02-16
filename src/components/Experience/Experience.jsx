3b4ca0cb0682aada131f717e6d7073cc9ebda432
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2026-02-16 17:13:20 +1000

    feat: experience card

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,6 +1,7 @@
 import EducationCard from "./EducationCard";
 import { useResume } from "../../hooks/resume";
 import { Spinner } from "@heroui/react";
+import ExperienceCard from "./ExperienceCard";
 
 export default function Experience() {
   const { data: resume, isPending } = useResume();
@@ -13,22 +14,17 @@ export default function Experience() {
         <h2 className="mb-2 text-xl cursor-pointer">🎓 Where I study</h2>
         <div className="flex flex-col gap-4">
           {resume.education.map((edu) => (
-            <EducationCard
-              key={edu.key}
-              education={edu}
-            />
+            <EducationCard key={edu.key} education={edu} />
           ))}
         </div>
       </section>
       <section className="w-full">
         <h2 className="mb-2 text-xl cursor-pointer">💼 What I've done</h2>
-        <ul>
-          <li className="w-full p-4 bg-white border border-gray-200 rounded-md">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
-            necessitatibus! Amet esse cupiditate saepe tenetur possimus qui ab?
-            Mollitia, aut.
-          </li>
-        </ul>
+        <div className="flex flex-col gap-4">
+          {resume.experience.map((exp, index) => (
+            <ExperienceCard key={index} experience={exp} />
+          ))}
+        </div>
       </section>
     </>
   );
